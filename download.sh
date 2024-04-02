77526ca740cdc858ac5ce6d275aaa6fce4fe7698
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2024-04-02 11:43:18 +0200

    Modify Download

--- download.sh
+++ download.sh
@@ -10,16 +10,16 @@ cd $start
 for month in {01..12}
 do 
 	echo "Getting $month"
-	if [ ! -f 2020-$month.tar.xz ]
-	then
-		wget https://zenodo.org/record/7650322/files/2020-$month.tar.xz?download=1
-		mv 2020-$month.tar.xz?download=1 2020-$month.tar.xz
-	fi
-	if [ ! -f 2022-$month.tar.xz ]
-	then
-		wget https://zenodo.org/record/7650318/files/2022-$month.tar.xz?download=1
-		mv 2022-$month.tar.xz?download=1 2022-$month.tar.xz
-	fi
+	# if [ ! -f 2020-$month.tar.xz ]
+	# then
+	# 	wget https://zenodo.org/record/7650322/files/2020-$month.tar.xz?download=1
+	# 	mv 2020-$month.tar.xz?download=1 2020-$month.tar.xz
+	# fi
+	# if [ ! -f 2022-$month.tar.xz ]
+	# then
+	# 	wget https://zenodo.org/record/7650318/files/2022-$month.tar.xz?download=1
+	# 	mv 2022-$month.tar.xz?download=1 2022-$month.tar.xz
+	# fi
 	if [ ! -f 2021-$month.tar.xz ]
 	then
 		wget https://zenodo.org/record/7650320/files/2021-$month.tar.xz?download=1
