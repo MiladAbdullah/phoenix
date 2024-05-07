334f2beae255bfb0dd8ce48ca45386ad022f3a84
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2024-05-07 16:32:13 +0200

    Create multiprocess functions

--- simulation/run.py
+++ simulation/run.py
@@ -25,21 +25,25 @@
 # harness that creates the simulation and then run it with the given scheduler, environment variables ... 
 # the simulation object created from the configuration
 
-from pathlib import Path
-from datetime import datetime
-from method import Method
-from verbose import Verbose
-from constants import METHOD_MAP
-import os
-import re
 import json
-from data import Data
+from multiprocessing import Process
+import os
+from pathlib import Path
+import re
+from typing import List
 
+
+# from local apps
+from simulation.data import Data, Sample
+from simulation.constants import METHOD_MAP
+from simulation.methods.pre_process.pre_process_method import PreProcessMethod
+from simulation.methods.verbose_method import VerboseMethod
+from simulation.verbose import Verbose
 
 class Simulation(Verbose):
     name: str
     source: Path
-    methods: dict[str, Method]
+    methods: dict[str, VerboseMethod]
     data: Data
     results: dict
     web: dict
@@ -68,35 +72,71 @@ class Simulation(Verbose):
         self.source = original_source
         
         self.methods = {}
-        for method_type, method_configuration in configuration["methods"].items():
+        for method_type, method_meta_data in configuration["methods"].items():
             if method_type in METHOD_MAP:
-                self.methods[method_type] = METHOD_MAP[method_type](method_configuration)
+                self.methods[method_type] = METHOD_MAP[method_type](method_meta_data)
+                
         
         self.data = Data(configuration["data"])
         self.results = configuration["results"] 
         self.os = configuration["os"]
         
         
+    
+        
     def __call__(self, *args, **kwargs):
         self.log_info(f"Simulation {self.name} started.")
         
         if "pre-process" in self.methods:
-            method = self.methods['pre-process']
-            self.log_info(f"Preprocessing with {method.name}")
-            processed_measurements = self.run(method, *args, **kwargs)
+            self.run_pre_process()
             
+            
+            
+    def run_pre_process(self):
+        method = self.methods['pre-process']
+            
+        # instancing the actual method
+        instance = method.classname(**method.configuration)
+        
+        # as dict
+        samples = [v for _, v in self.data.samples.items()]
+
+        def update_sample_with_pre_processed_data(old_samples: List[Sample]) -> None:
+            for sample in old_samples:
+                pre_processed_measurements = []
+                
+                for measurement in sample.measurements:
+                    new_path = instance.process(measurement)
+                    
+                    if new_path is not None:
+                        pre_processed_measurements.append(new_path)
+                        
+                new_sample = sample
+                new_sample.measurements = pre_processed_measurements
+                new_sample.count = len(pre_processed_measurements)
+                self.data.update_sample(sample, new_sample)
+        
+        self.run(samples, update_sample_with_pre_processed_data, self.os["process_count"], caption="pre processing")
+        
+
+    def run (self, samples: List[Sample], function: callable, process_count: int, caption: str = ""):
+        
+        batch = max(1, len(samples) // (process_count - 1))
+
+        processes = [      
+            Process(target=function, args=(samples[i*batch: min((i+1)*batch, len(samples))],)) 
+            for i in range(process_count)]
+        
+        for i, process in enumerate(processes):
+            process.start()
+            
+        for i, process in enumerate(processes):
+            process.join()
+
     
-    # returns list of database measurements
-    def run(self, method, *args, **kwargs) -> :
-        
-        
-            
-            
-        
-            
-        
 if __name__ == "__main__":
-    with open("config-template.json", "r") as configure_file:
+    configuration_file = Path()  / os.environ.get("PHOENIX_HOME") / "simulation/config-template.json"
+    with open(configuration_file, "r") as configure_file:
         configuration = json.loads(configure_file.read())
         
     xs = Simulation(configuration=configuration)
