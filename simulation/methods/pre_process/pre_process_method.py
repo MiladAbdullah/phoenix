c2b341b9d8c902249d5653e001e58212ed3561a1
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2024-05-07 16:37:03 +0200

    Fix includes

--- simulation/methods/pre_process/pre_process_method.py
+++ simulation/methods/pre_process/pre_process_method.py
@@ -1,14 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+Generic class for all pre-process methods
+"""
 from pathlib import Path
-from typing import Any
 import pandas as pd
-from methods.verbose_method import VerboseMethod
+from typing import Any
+
+# local apps
+from simulation.methods.verbose_method import VerboseMethod
 
 class PreProcessMethod(VerboseMethod):
 
     def __init__(self, method_name: str = "PreProcessMethod") -> None:
         super().__init__(method_name=method_name)
 
-    def process(self, *args: Any, **kwargs: Any) -> pd.DataFrame:
+    def process(self, *args: Any, **kwargs: Any) -> pd.DataFrame|str|Path:
         raise NotImplementedError
     
    
