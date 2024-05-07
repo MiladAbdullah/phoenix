dafa453c7f3ca3e4a290f0ad92251a61307229fa
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2024-05-07 16:36:28 +0200

    Add names to all Verbose Models

--- simulation/methods/verbose_method.py
+++ simulation/methods/verbose_method.py
@@ -1,12 +1,12 @@
 import logging
 from typing import Any
-from methods.generic_method import GenericMethod
+from simulation.methods.generic_method import GenericMethod
 
 class VerboseMethod(GenericMethod):
         
     verbose: bool
     logger: logging.Logger
-    
+
     def __init__(self, method_name:str) -> None:
         self.logger = logging.getLogger(method_name)
         formatter = logging.Formatter('%(asctime)s; %(message)s')
@@ -14,7 +14,7 @@ class VerboseMethod(GenericMethod):
         stream_handler.setFormatter(formatter)
         self.logger.addHandler(stream_handler)
         
-        super().__init__()
+        super().__init__(name=method_name)
     
     def log_info(self, message: str):
         if self.verbose:
