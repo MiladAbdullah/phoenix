09ccd288b7534df95a5e7e93dd9fd3238bb2fe69
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2024-05-07 16:35:54 +0200

    Fix includes

--- simulation/method.py
+++ simulation/method.py
@@ -1,15 +1,16 @@
 import importlib
-from methods.verbose_method import VerboseMethod
 from typing import Type
 from types import ModuleType
 
 class Method:
     name: str
-    method: Type[VerboseMethod]
+    classname: Type
     module: ModuleType
+    configuration: dict
     
-    def __init__(self, name) -> None:
+    def __init__(self, name, configuration:dict) -> None:
         self.name = name
+        self.configuration = configuration
     
     
     def load_class(module_name, class_name):
@@ -17,12 +18,12 @@ class Method:
         class_ = getattr(module, class_name)
         return class_
 
-    def set_method_by_name(self, class_name: str) -> Type[VerboseMethod]:
+    def set_class_by_name(self, class_name: str) -> None:
         components = class_name.split('_')
         ClassName = components[0][0].upper() + components[0][1:] + ''.join(x.title() for x in components[1:])
         assert ClassName in  self.module.__dict__, f"no class named {ClassName} found in {self.module.__name__}."
         
-        self.method = getattr(self.module, ClassName)
+        self.classname = getattr(self.module, ClassName)
     
         
     def __str__(self) -> str:
@@ -31,9 +32,10 @@ class Method:
 
 class PreProcessMethod(Method):
     
-    def __init__(self, configuration: dict) -> None:
-        assert "method" in  configuration, "method keyword not found in configuration"
-        self.module = importlib.import_module(f"methods.pre_process.{configuration['method']}")
-        self.set_method_by_name(configuration["method"])
-        super().__init__(name="pre-process")
+    def __init__(self, meta_data: dict) -> None:
+        assert "method" in  meta_data, "method keyword not found in configuration"
+        self.module = importlib.import_module(f"methods.pre_process.{meta_data['method']}")
+        self.set_class_by_name(meta_data["method"])
+        
+        super().__init__(name="pre-process",  configuration=meta_data['configuration'])
 
