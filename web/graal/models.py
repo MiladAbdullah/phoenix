67da384aae749c9805f820415219bd008598d983
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2024-05-14 00:10:21 +0200

    Change Comparison model

--- web/graal/models.py
+++ web/graal/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 
+
 class MachineType(models.Model):
     name = models.CharField(max_length=128)
     label = models.CharField(max_length=128, blank=True)
     description = models.TextField(blank=True)
 
-    def __str__ (self):
+    def __str__(self):
         return f"{self.id} - {self.name}, {self.description}"
 
 
 class MachineHost(models.Model):
     machine_type = models.ForeignKey(MachineType, on_delete=models.CASCADE) 
 
-    def __str__ (self):
+    def __str__(self):
         return f"{self.id} - {self.machine_type.id}, {self.machine_type.name}"
 
 
@@ -21,7 +22,7 @@ class Configuration(models.Model):
     label = models.CharField(max_length=128, blank=True)
     description = models.TextField(blank=True)
     
-    def __str__ (self):
+    def __str__(self):
         return f"{self.id} - {self.name}, {self.description}"
 
 
@@ -30,18 +31,16 @@ class BenchmarkType(models.Model):
     label = models.CharField(max_length=128, blank=True)
     description = models.TextField(blank=True)
     
-    def __str__ (self):
+    def __str__(self):
         return f"{self.id} - {self.name}, {self.description}"
-
 
 
 class BenchmarkWorkload(models.Model):
     name = models.CharField(max_length=255)
     benchmark_type = models.ForeignKey(BenchmarkType, on_delete=models.CASCADE)
 
-    def __str__ (self):
+    def __str__(self):
         return f"{self.id} - {self.name}, {self.benchmark_type.id}, {self.benchmark_type.name}"
-
 
 
 class PlatformType(models.Model):
@@ -49,18 +48,15 @@ class PlatformType(models.Model):
     label = models.CharField(max_length=128, blank=True)
     description = models.TextField(blank=True)
 
-    def __str__ (self):
+    def __str__(self):
         return f"{self.id} - {self.name}, {self.description}"
-
-
 
 
 class Repository(models.Model):
     name = models.CharField(max_length=128)
 
-    def __str__ (self):
+    def __str__(self):
         return f"{self.id} - {self.name}"
-
 
 
 class Version(models.Model):
@@ -69,16 +65,15 @@ class Version(models.Model):
     tag = models.CharField(max_length=128, blank=True)
     repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
     
-    def __str__ (self):
+    def __str__(self):
         return f"{self.id} - {self.commit}, {self.datetime}, {self.repository.id}, {self.repository.name}"
-
 
 
 class PlatformInstallation(models.Model):
     platform_type = models.ForeignKey(PlatformType, on_delete=models.CASCADE)
     version = models.ForeignKey(Version, on_delete=models.CASCADE)
 
-    def __str__ (self):
+    def __str__(self):
         return f"{self.id} - {self.platform_type.id}, {self.platform_type.name}, {self.version.id}"
 
 
@@ -90,24 +85,26 @@ class Measurement(models.Model):
     configuration = models.ForeignKey(Configuration, on_delete=models.CASCADE)
     measurement_directory = models.URLField()
 
-    def __str__ (self):
+    def __str__(self):
         return f"{self.id} - {self.measurement_directory}"
-
 
     def get_meta(self):
         return f"{self.machine_host.machine_type.id}-{self.configuration.id}" \
             + f"-{self.benchmark_workload.id}-{self.platform_installation.id}"
 
+
 class Comparison(models.Model):
-    measurement_old_count = models.SmallIntegerField(default=33)    
-    measurement_new_count = models.SmallIntegerField(default=33)    
+    key = models.CharField(max_length=256, unique=True)
+    real_id = models.PositiveBigIntegerField(default=0)
+    measurement_old_count = models.SmallIntegerField(default=33)
+    measurement_new_count = models.SmallIntegerField(default=33)
     measurement_old = models.ForeignKey(Measurement, on_delete=models.CASCADE, related_name="old")
     measurement_new = models.ForeignKey(Measurement, on_delete=models.CASCADE, related_name="new")
     regression = models.BooleanField(default=False)
     p_value = models.DecimalField(default=0, decimal_places=14, max_digits=20)
     size_effect = models.DecimalField(default=0, decimal_places=4, max_digits=10)
-    metric = models.CharField(max_length=128, default="iteration_time_ns")
+    column = models.CharField(max_length=128, default="iteration_time_ns")
     generated = models.BooleanField(default=False)
     
-    def __str__ (self):
-        return f"{self.id} "
+    def __str__(self):
+        return f"{self.id}"
