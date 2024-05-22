b91e23df58a3b11d81f434eaa3e249b81a7f63d4
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2024-05-22 12:07:24 +0200

    Add new wrappers

--- simulation/wrapper.py
+++ simulation/wrapper.py
@@ -5,13 +5,22 @@ from types import ModuleType
 
 class Wrapper:
     name: str
-    classname: Type
+    classname: Type | None = None
     module: ModuleType
     conf: dict
     
     def __init__(self, name, conf: dict) -> None:
         self.name = name
         self.conf = conf
+
+    def create_method(self, verbose: bool, new_configuration: dict = None, **kwargs):
+        if self.classname is None:
+            raise Exception("call set_class_by_name first.")
+
+        if new_configuration is not None:
+            return self.classname(verbose=verbose, **new_configuration, **kwargs)
+
+        return self.classname(verbose=verbose, **self.conf, **kwargs)
 
     @staticmethod
     def load_class(module_name, class_name):
@@ -29,14 +38,35 @@ class Wrapper:
 
     def __str__(self) -> str:
         return f"Method: {self.name}, assigned to {self.module.__name__} for class {self.classname}"
-        
 
-class PreProcessWrapper(Wrapper):
+
+class SimpleWrapper(Wrapper):
+
+    def __init__(self, method_name: str, meta_data: dict, category: str) -> None:
+        assert "method" in meta_data, "method keyword not found in configuration"
+        self.module = importlib.import_module(f"methods.{category}.{meta_data['method']}")
+        self.set_class_by_name(meta_data["method"])
+
+        super().__init__(name=method_name, conf=meta_data['configuration'])
+
+
+class PreProcessWrapper(SimpleWrapper):
     
     def __init__(self, meta_data: dict) -> None:
-        assert "method" in meta_data, "method keyword not found in configuration"
-        self.module = importlib.import_module(f"methods.pre_process.{meta_data['method']}")
-        self.set_class_by_name(meta_data["method"])
-        
-        super().__init__(name="pre-process",  conf=meta_data['configuration'])
+        super().__init__(method_name="pre-process", meta_data=meta_data, category="pre_process")
 
+
+class FrequencyWrapper(SimpleWrapper):
+
+    def __init__(self, meta_data: dict) -> None:
+        super().__init__(method_name="frequency", meta_data=meta_data, category="frequency")
+
+
+class LimitWrapper(SimpleWrapper):
+    def __init__(self, meta_data: dict) -> None:
+        super().__init__(method_name="limit", meta_data=meta_data, category="limit")
+
+
+class DetectionWrapper(SimpleWrapper):
+    def __init__(self, meta_data: dict) -> None:
+        super().__init__(method_name="detection", meta_data=meta_data, category="detection")
