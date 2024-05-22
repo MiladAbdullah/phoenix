53632a9148c74b7b1f573bc5ce51f7b6abb44826
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2024-05-22 12:06:42 +0200

    Add verbose for method

--- simulation/methods/pre_process/pre_process_method.py
+++ simulation/methods/pre_process/pre_process_method.py
@@ -10,10 +10,11 @@ from typing import Any
 # local apps
 from simulation.methods.verbose_method import VerboseMethod
 
+
 class PreProcessMethod(VerboseMethod):
 
-    def __init__(self, method_name: str = "PreProcessMethod") -> None:
-        super().__init__(method_name=method_name)
+    def __init__(self, method_name: str = "PreProcessMethod", verbose: bool = False) -> None:
+        super().__init__(method_name=method_name, verbose=verbose)
 
     def process(self, *args: Any, **kwargs: Any) -> pd.DataFrame|str|Path:
         raise NotImplementedError
