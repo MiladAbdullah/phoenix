e85f83e3f1d1a3f0df3e7b298500ed283bd8d7e3
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2024-05-22 12:07:12 +0200

    Add verbose initilization

--- simulation/methods/verbose_method.py
+++ simulation/methods/verbose_method.py
@@ -7,13 +7,14 @@ class VerboseMethod(GenericMethod):
     verbose: bool
     logger: logging.Logger
 
-    def __init__(self, method_name:str) -> None:
+    def __init__(self, method_name: str, verbose: bool) -> None:
         self.logger = logging.getLogger(method_name)
         formatter = logging.Formatter('%(asctime)s; %(message)s')
         stream_handler = logging.StreamHandler()
         stream_handler.setFormatter(formatter)
         self.logger.addHandler(stream_handler)
-        
+        self.verbose = verbose
+
         super().__init__(name=method_name)
     
     def log_info(self, message: str):
@@ -24,17 +25,17 @@ class VerboseMethod(GenericMethod):
     def log_debug(self, message: str, *args: Any, **kwargs: Any):
         if self.verbose:
             self.logger.setLevel(logging.DEBUG)
-            self.logger.debug(message, *args , **kwargs)
+            self.logger.debug(f"{self.name} -> {message}", *args , **kwargs)
                 
     def log_warning(self, message: str):
         if self.verbose:
             self.logger.setLevel(logging.WARNING)
-            self.logger.warning(message)
+            self.logger.warning(f"{self.name} -> {message}")
             
     def log_error(self, message: str, *args: Any, **kwargs: Any):
         self.logger.setLevel(logging.ERROR)
-        self.logger.error(message, *args , **kwargs)
+        self.logger.error(f"{self.name} -> {message}", *args , **kwargs)
         
     def log_error_critical(self, message: str, *args: Any, **kwargs: Any):
         self.logger.setLevel(logging.CRITICAL)
-        self.logger.error(message, *args , **kwargs)
+        self.logger.error(f"{self.name} -> {message}", *args , **kwargs)
