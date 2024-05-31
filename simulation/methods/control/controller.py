92a21a4e62ba7b4ca103946b854fcbffe7ac53fe
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2024-05-31 10:14:29 +0200

    Add generic Control

---
+++ simulation/methods/control/controller.py
@@ -0,0 +1,12 @@
+from simulation.data import Sample
+from simulation.methods.verbose_method import VerboseMethod
+
+
+class Controller(VerboseMethod):
+	benchmark_configurations: dict
+
+	def __init__(self, method_name: str, verbose: bool):
+		super().__init__(method_name=method_name, verbose=verbose)
+
+	def control(self, key: str, sample_pairs: list[tuple[Sample, Sample]], column: str) -> dict[str, dict]:
+		raise NotImplementedError
