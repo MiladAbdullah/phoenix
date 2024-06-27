1f45d8c8b2ed3cbea79684b09211e0b9a884f356
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-06-27 17:08:23 +0200

    Start implementation of Peass method call

--- simulation/methods/control/peass.py
+++ simulation/methods/control/peass.py
@@ -1,10 +1,74 @@
+import datetime
+import json
+import os
+import random
+from pathlib import Path
+from typing import Callable
 from simulation.data import Sample
 from simulation.methods.control.controller import Controller
+from simulation.methods.detection.bootstrap_change_point_detector import BootstrapChangePointDetector
 
+SPLIT_METHODS = ["by-days", "by-commits", "by-date"]
 
 class Peass(Controller):
-	def __init__(self, verbose: bool = False):
-		super().__init__(verbose=verbose, method_name="PEASSController")
+	start_date: datetime.date
+	end_date: datetime.date
+	type2error: float
+	is_training_sample: Callable[[Sample, datetime.date, int], bool] | None
+
+	def __init__(
+			self,
+			verbose: bool,
+			train_text: str,
+			start_date: datetime.date,
+			end_date: datetime.date,
+			type2error: float) -> None:
+
+		super().__init__(method_name="PEASSController", verbose=verbose)
+
+		self.cache_path = Path() / os.getenv("PHOENIX_HOME", ".") / "_cache/peass"
+		self.start_date = start_date
+		self.end_date = end_date
+		self.is_training_sample = self.parse_train_text(train_text)
+		self.type2error = type2error
+
+	def parse_train_text(self, train_text) -> Callable[[Sample, datetime.date, int], bool] | None:
+		try:
+			split_method, indicator = train_text.split(',')
+		except ValueError:
+			self.log_error(f"not enough values to unpack (expected 2) from {train_text} as <method>,<value>")
+			return None
+
+		try:
+			if split_method == "by-date":
+				indicator = datetime.datetime.strptime(indicator, "%d-%m-%Y").date()
+			else:
+				indicator = int(indicator)
+				if indicator <= 0:
+					self.log_error(f"number of days should be higher than 0: current is {indicator}")
+					return None
+
+		except ValueError:
+			self.log_error(f"unable to convert to int or date [dd-mm-yyyy], current value is {indicator}")
+			return None
+
+		if split_method not in SPLIT_METHODS:
+			self.log_error(f"Unknown {split_method}, choose from {SPLIT_METHODS}")
+			return None
+
+		def is_training_sample(sample: Sample, first_date: datetime.date, current_index: int = 0) -> bool:
+			if split_method == "by-count":
+				return current_index < indicator
+
+			elif split_method == "by-days":
+				return (sample.measurement.platform_installation.version.datetime.date() - first_date).days < indicator
+
+			elif split_method == "by-date":
+				return sample.measurement.platform_installation.version.datetime.date() < indicator
+
+		return is_training_sample
+
+#	def train(self, meta_key: str, sample: Sample, column: str):
 
 	def control(self, meta_key: str, sample_pairs: list[tuple[Sample, Sample]], column: str) -> dict[str, dict]:
 		
@@ -130,4 +194,38 @@ class Peass(Controller):
 		# The evalaution and the rest can be done here
 		
 
-		raise NotImplementedError
+		first_sample_date = sample_pairs[0][0].measurement.platform_installation.version.datetime.date()
+		results = {}
+
+		for i, sample_pair in enumerate(sample_pairs):
+			old_sample, new_sample = sample_pair
+			old_key, new_key = old_sample.get_meta_key(), new_sample.get_meta_key()
+			database_key = f"{old_key}:{new_key}:{column}"
+
+			try:
+				array1 = old_sample.get_data(column, self.log_error)
+				array2 = new_sample.get_data(column, self.log_error)
+			except KeyError:
+				results[database_key] = {}
+				continue
+
+			bootstrap_detector = BootstrapChangePointDetector(verbose=self.verbose, method_name="MutationComparer")
+			bootstrap_detector.boots = 350
+			bootstrap_detector.p_value_threshold = 0.01
+
+			results[database_key] = {}
+
+			old_runs = [random.choice(array1) for _ in range(30)]
+			new_runs = [random.choice(array2) for _ in range(30)]
+			compared = bootstrap_detector.compute_difference_one_per_rep(old_runs, new_runs)
+
+			if len(compared) > 0:
+				if compared['p_value'] > 0.01:
+					# stop experiment, no regression
+					compared['regression'] = False
+					results[database_key] = compared
+					break
+				else:
+					continue
+
+		return results
